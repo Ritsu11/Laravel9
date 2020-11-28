e85d647e5730f1c1beeee04c71ec270dd66a5da8
ishimigne_note <garethbale3113@gmail.com>
2020-11-28 19:26:41 +0900

    Add Model and Validate

--- server/app/Http/Controllers/PersonController.php
+++ server/app/Http/Controllers/PersonController.php
@@ -20,8 +20,27 @@ class PersonController extends Controller
 
     public function search(Request $request)
     {
-        $item = Person::where('name', $request->input)->first();
+        $min = $request->input * 1;
+        $max = $min + 10;
+        $item = Person::ageGreaterThan($min)
+            ->ageLessThan($max)
+            ->first();
         $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
+    }
+
+    public function add(Request $request)
+    {
+        return view('person.add');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Person::$rules);
+        $person = new Person;
+        $form = $request->all();
+        unset($form['_token']);
+        $person->fill($form)->save();
+        return redirect('/person');
     }
 }
